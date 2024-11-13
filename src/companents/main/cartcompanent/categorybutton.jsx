9d2e0291521961591ac7079a4b825d1080644d68
RailJannatov <railcennetov@gmail.com>
2024-11-13 16:18:54 +0400

    test

--- src/companents/main/cartcompanent/categorybutton.jsx
+++ src/companents/main/cartcompanent/categorybutton.jsx
@@ -1,60 +1,60 @@
 import React, { useReducer, useEffect } from "react";
 import { cartArray } from "./cartfiles";
 
+// const initialState = { take: "all" };
 
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "get-men":
+//       return { take: "men" };
+//     case "get-woman":
+//       return { take: "woman" };
+//     case "get-jewelery":
+//       return { take: "jewelry" };
+//     case "get-electronic":
+//       return { take: "electronic" };
+//     case "get-all":
+//       return { take: "all" };
+//       default:
 
+//   }
+// }
 
+function CategoryButton({ setCard }) {
+  // const [state, dispatch] = useReducer(reducer, initialState);
 
-const initialState = { take: "all" };
+  const handleFilter = (type) => {
+    if (type !== "all") {
+      setCard(cartArray.filter((card) => card.type === type));
+    } else {
+      setCard(cartArray);
+    }
+  };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "get-men":
-      return { take: "men" };
-    case "get-woman":
-      return { take: "woman" };
-    case "get-jewelery":
-      return { take: "jewelry" };
-    case "get-electronic":
-      return { take: "electronic" };
-    case "get-all":
-      return { take: "all" };
-      default:
-      state;
-  }
-}
-
-function CategoryButton({takelement}) {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  useEffect(()=>{
-    let filtercart;
-  if(state.take==='all'){
-    filtercart=cartArray
-  }else{
-    filtercart=cartArray.filter((card)=>card.type===state.take)
-  }
-  takelement(filtercart);
-  },[state.take]);
-  
+  // useEffect(()=>{
+  //   let filtercart;
+  // if(state.take==='all'){
+  //   filtercart=cartArray
+  // }else{
+  //   filtercart=cartArray.filter((card)=>card.type===state.take)
+  // }
+  // takelement(filtercart);
+  // },[state.take]);
 
   return (
     <div className="fives-button">
-      <button onClick={()=>dispatch({type:"get-all"})}>All</button>
+      {/* <button onClick={()=>dispatch({type:"get-all"})}>All</button>
       <button onClick={()=>dispatch({type:"get-men"})}>Men's Clothing</button>
       <button onClick={()=>dispatch({type:"get-woman"})}>Women's Clothing</button>
       <button onClick={()=>dispatch({type:"get-jewelery"})}>Jewelery</button>
-      <button onClick={()=>dispatch({type:"get-electronic"})}>Electronics</button>
+      <button onClick={()=>dispatch({type:"get-electronic"})}>Electronics</button> */}
+      <button onClick={() => handleFilter("all")}>All</button>
+      <button onClick={() => handleFilter("men")}>Men's Clothing</button>
+      <button onClick={() => handleFilter("woman")}>Women's Clothing</button>
+      <button onClick={() => handleFilter("jewelry")}>Jewelery</button>
+      <button onClick={() => handleFilter("electronic")}>Electronics</button>
     </div>
   );
 }
 
 export default CategoryButton;
-
-
-
-
-
-
-
-
--- src/companents/main/product.jsx
+++ src/companents/main/product.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import CartGroup from "./cartcompanent/cartgroup";
+import { cartArray } from "./cartcompanent/cartfiles";
 import CategoryButton from "./cartcompanent/categorybutton";
 
 function ProductSection() {
-  const [cart, setcart] = useState([]);
+  const [cart, setCart] = useState([...cartArray]);
+
+  console.log(cart);
+
+
 
   return (
     <section>
       <div className="container">
         <div className="product-group">
           <div className="shopping-button">
-            <CategoryButton takelement={setcart} />
+            <CategoryButton setCard={setCart} />
           </div>
           <CartGroup cartitem={cart} />
         </div>
